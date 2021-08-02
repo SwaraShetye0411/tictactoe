5f0586b147eeb4951914add331077a44a95ede9a
SwaraShetye <swarashetye0411@gmail.com>
2021-08-02 20:51:26 +0530

    highlight winning squares and add start new game button

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 
-const Square = ({value, onClick}) => {
+const Square = ({value, onClick, isWinningSquares}) => {
+    
     return (
-        <button type="button" className="square" onClick={onClick}>{value}</button>
+        <button type="button" className="square" onClick={onClick} style={{fontWeight: isWinningSquares? 'bold':'normal'}}>{value}</button>
     )
 }
 

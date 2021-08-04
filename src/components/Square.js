2512b7de4fa114b608e4637960a92c8d51112add
SwaraShetye <swarashetye0411@gmail.com>
2021-08-04 17:09:37 +0530

    styled the entire app

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Square = ({value, onClick, isWinningSquares}) => {
     
     return (
-        <button type="button" className="square" onClick={onClick} style={{fontWeight: isWinningSquares? 'bold':'normal'}}>{value}</button>
+        <button type="button" className={`square ${isWinningSquares? 'winning': ''} ${value === 'X'?'text-green':'text-orange'}`} onClick={onClick}>{value}</button>
     )
 }
 
